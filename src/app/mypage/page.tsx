603454dd007678f72b1e5aa491c1d3c746d22ca5
kewii33 <kewiii071@gmail.com>
2024-03-15 22:30:47 +0900

    [feat/#12] - mypage image 등록 생성

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const myPage = () => {
-  return <div>마이페이지입니다~~!!!!</div>;
+  return <div>마이페이지입니다~~!!!?fjasklfjasklfmsldkfmslfksdfl</div>;
 };
 
 export default myPage;
