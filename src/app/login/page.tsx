02cf389ce771167c92ae059ddc72acbf655d14e4
yeji Sung <dpwl0332@naver.com>
2024-03-15 23:01:11 +0900

    [feat/#5] - login/page.tsx 파일 생성

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <p>로그인 페이지 생성</p>
+      <p>issue & PR test</p>
+    </div>
+  );
+};
+
+export default LoginPage;
